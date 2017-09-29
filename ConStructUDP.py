5a80e551666f048da05b2d9d5174f09206938a6c
remy <remy@localhost.localdomain>
2017-09-28 23:08:29 -0400

    linting

--- ConStructUDP.py
+++ ConStructUDP.py
@@ -4,6 +4,8 @@ import json
 import sys
 
 # this class will handle the udp socket
+
+
 class Connection:
 
     def __init__(self, ip, port):
@@ -11,15 +13,15 @@ class Connection:
 
         self.__ip = ip
         self.__port = port
-        
+
     def __createSocket(self):
 
         # declare an IP and UDP socket
-        self.__socket = socket.socket(socket.AF_INET, \
-            socket.SOCK_DGRAM)
+        self.__socket = socket.socket(socket.AF_INET,
+                                      socket.SOCK_DGRAM)
 
     def sendSocket(self, struct_data):
-        print "Sending to: ", self.__ip,":",self.__port
+        print "Sending to: ", self.__ip, ":", self.__port
         self.__socket.connect((self.__ip, self.__port))
 
         self.__socket.sendall(struct_data)
@@ -53,11 +55,11 @@ class PackStruct:
 
 
 class DataTypes:
- 
-    def __init__(self):
-    #hash table of input data types translated to struct string
 
-       self.__data_types = {
+    def __init__(self):
+        # hash table of input data types translated to struct string
+
+        self.__data_types = {
 
             "char": "c",
             "signed char": "b",
@@ -80,33 +82,34 @@ class DataTypes:
         if s in self.__data_types.keys():
 
             # if we get a length then its a char array so format with length
-            if l :
+            if l:
                 # return formatted map
                 return self.__data_types[s].format(l)
-            #otherwise return lookup
+            # otherwise return lookup
             return self.__data_types[s]
         else:
             # there was something invalid in the struct json file
             raise ValueError("invalid struct field", s)
 
+
 def main():
 
     global no_unicode
 
-    #open json file will be unicode
+    # open json file will be unicode
     with open(sys.argv[1]) as data_file:
         data = json.load(data_file)
 
-    #create connection
+    # create connection
     con = Connection(data["ip"], data["port"])
     types = DataTypes()
 
-    parse_str =''
+    parse_str = ''
     vals = []
     for key in data["struct"]:
 
-        #encode from unicode, struct doesnt take unicode
-        field = key.keys()[0].encode('ascii','ignore')
+        # encode from unicode, struct doesnt take unicode
+        field = key.keys()[0].encode('ascii', 'ignore')
 
         # if we have a char then convert and call lookup data type
         if field in ('char'):
@@ -125,7 +128,7 @@ def main():
             parse_str += types.Lookup(field)
 
     # pack the binary data with ! network order
-    packed = PackStruct("!"+parse_str, vals)
+    packed = PackStruct("!" + parse_str, vals)
 
     print "Size of Packed Data: ", packed.sizeOfPacked()
     print "Packed Data: ", packed.getUnPacked()
@@ -133,7 +136,5 @@ def main():
     con.sendSocket(packed.getPacked())
 
 
-
 if __name__ == "__main__":
     main()
-
